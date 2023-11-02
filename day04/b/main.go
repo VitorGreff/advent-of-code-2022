192513e7a000b8d88e5266d11e7f6401d55f4cee
VitorGreff <vitorgreffdias@gmail.com>
2023-11-02 13:33:51 -0300

    day 04

---
+++ day04/b/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	var counter uint = 0
+
+	file, err := os.Open("../4.txt")
+	if err != nil {
+		log.Fatal("Error opening the file")
+	}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		counter += checkPair(scanner.Text())
+	}
+	fmt.Println(counter)
+}
+
+func checkPair(line string) uint {
+	v := strings.Split(line, ",")
+	c1 := strings.Split(v[0], "-")
+	c2 := strings.Split(v[1], "-")
+
+	return evaluate(c1, c2)
+}
+
+func evaluate(c1 []string, c2 []string) uint {
+	n1, err1 := strconv.Atoi(c1[0])
+	n2, err2 := strconv.Atoi(c1[1])
+	n3, err3 := strconv.Atoi(c2[0])
+	n4, err4 := strconv.Atoi(c2[1])
+	if err1 != nil || err2 != nil || err3 != nil || err4 != nil {
+		log.Fatal("Error converting values")
+	}
+
+	if n2 >= n3 && n1 <= n4 {
+		return 1
+	}
+	return 0
+}
