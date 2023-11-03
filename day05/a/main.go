d7cd9dadad054fb328276ea6a785e185a046bbaf
VitorGreff <vitorgreffdias@gmail.com>
2023-11-02 23:46:05 -0300

    working solution, probably will refactor it

--- day05/a/main.go
+++ day05/a/main.go
@@ -6,19 +6,13 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 )
 
 type stack []string
 
-var stack1 = stack{}
-var stack2 = stack{}
-var stack3 = stack{}
-
-var stacks = map[int]*stack{
-	1: &stack1,
-	2: &stack2,
-	3: &stack3,
-}
+var stacks []stack
+var aux int = 0
 
 func (s *stack) push(v string) {
 	*s = append(*s, v)
@@ -35,47 +29,70 @@ func (s *stack) pop() string {
 }
 
 func main() {
-	file, err := os.Open("../5.txt")
+	var path string = "../5.txt"
+	checkStackNumber(path)
+
+	file, err := os.Open(path)
 	if err != nil {
 		log.Fatal("Error opening the file")
 	}
+
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() && scanner.Text() != "" {
-		for string(scanner.Text()[1]) != "1" {
-			if scanner.Text()[1] != ' ' {
-				readFileToStack(&stack1, string(scanner.Text()[1]))
+		if string(scanner.Text()[1]) != "1" {
+			for i := 1; i < len(scanner.Text())+1; i += 4 {
+				readFileToStack(&stacks[aux], string(scanner.Text()[i]))
+				aux++
 			}
-			if scanner.Text()[5] != ' ' {
-				readFileToStack(&stack2, string(scanner.Text()[5]))
-			}
-			if scanner.Text()[9] != ' ' {
-				readFileToStack(&stack3, string(scanner.Text()[9]))
-			}
-			scanner.Scan()
+			aux = 0
 		}
 	}
 	for scanner.Scan() {
-		index1, err1 := strconv.Atoi(string(scanner.Text()[5]))
-		index2, err2 := strconv.Atoi(string(scanner.Text()[12]))
-		index3, err3 := strconv.Atoi(string(scanner.Text()[17]))
+		s := strings.Split(scanner.Text(), " ")
+		index1, err1 := strconv.Atoi(string(s[1]))
+		index2, err2 := strconv.Atoi(string(s[3]))
+		index3, err3 := strconv.Atoi(string(s[5]))
 		if err1 != nil || err2 != nil || err3 != nil {
 			log.Fatal("Error parsing instructions")
 		}
 		evaluateInstructions(index1, index2, index3)
 	}
-	fmt.Printf("End of first stack: %s\n", stack1[len(stack1)-1])
-	fmt.Printf("End of second stack: %s\n", stack2[len(stack2)-1])
-	fmt.Printf("End of third stack: %s", stack3[len(stack3)-1])
+	fmt.Println(stacks)
+}
 
+func readFileToStack(stack *stack, str string) {
+	if str != " " {
+		*stack = append([]string{str}, *stack...)
+	}
+}
+
+func checkStackNumber(path string) {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal("Error opening the file")
+	}
+	scanner := bufio.NewScanner(file)
+	defer file.Close()
+	for scanner.Scan() && scanner.Text() != "" {
+		if line := string(scanner.Text()); string(line[1]) == "1" {
+			n, err := strconv.Atoi(string(line[len(line)-2]))
+			instantiateStacks(n)
+			if err != nil {
+				log.Fatal("Error declaring the number of stacks")
+			}
+		}
+	}
+}
+
+func instantiateStacks(n int) {
+	for i := 0; i < n; i++ {
+		stacks = append(stacks, stack{})
+	}
 }
 
 func evaluateInstructions(inst1, inst2, inst3 int) {
 	for i := 0; i < inst1; i++ {
-		stacks[inst3].push(stacks[inst2].pop())
+		stacks[inst3-1].push(stacks[inst2-1].pop())
 	}
-}
-
-func readFileToStack(stack *stack, str string) {
-	*stack = append([]string{str}, *stack...)
 }
